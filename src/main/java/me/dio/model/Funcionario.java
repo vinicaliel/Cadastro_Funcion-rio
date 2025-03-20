d692416afb363341b54525d9acdd8f997a10ec26
Caliel Passos <caliel1023@yahoo.com>
2025-03-20 14:07:27 -0300

    feat: getters and setters

--- src/main/java/me/dio/model/Funcionario.java
+++ src/main/java/me/dio/model/Funcionario.java
@@ -3,15 +3,53 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 
 @Entity(name = "funcionarios")
 public class Funcionario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private String cargo;
     @Column(name = "salario", nullable = false, precision = 10, scale = 2)
     private BigDecimal salario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
 
 
 }
